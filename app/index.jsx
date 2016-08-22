65a3271192561bbf167784a9ce726329cf9a0fe9
Matt Stuhring <whereyouatmatt@gmail.com>
2016-08-22 10:18:14 -0700

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,27 @@
+import { blue500, blue700 } from 'material-ui/styles/colors';
+import Routes from 'components/Routes';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  flatButton: {
+    buttonFilterColor: blue700,
+    textColor: 'white'
+  },
+
+  palette: {
+    primary1Color: blue500
+  }
+});
+
+ReactDOM.render(
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Routes />
+  </MuiThemeProvider>,
+  document.getElementById('app')
+);
